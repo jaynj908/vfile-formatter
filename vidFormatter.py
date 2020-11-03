2bf837ba1dc22ec8068ba69534f5be2abbefea1c
Jay Lynch <jaynn_sp@yahoo.com>
2020-11-03 12:52:52 -0500

    rewrote script will add defs later

--- vidFormatter.py
+++ vidFormatter.py
@@ -1,59 +1,87 @@
 import os
 
-def remake(x):
-    print('Will be renamed==>', x)
+# Set the folder to be formatted
+os.chdir(r'E:\SHOWS\LOVE CHUNIBYO')
 
-#Open the foler and get a list of files. Split the file and file extension
-def nameCheck():
-    for f in os.listdir():
-        fileName, fileExt = os.path.splitext(f)
-        fileName = fileName.strip()
-        for word in omitWordList:
-            fileName = fileName.replace(word, '')
+# List of strings to be removed from each filename
+omit = ['English', 'Dubbed',  'Watch', 'cartoons', 'online',
+        'Watch', 'anime', 'online', 'English', 'dub', 'anime']
 
-        #Split the fileName and Episode number. 
-        show, episodeNum = fileName.split('Episode')
-        #Strip away empty space and fill episodeNum with a zero so shows will play correctly in a playlist
-        show, episodeNum, fileExt = show.strip(), episodeNum.strip().zfill(2),fileExt.strip()
-        #Set the format you want the shows to appear
-        newName = f'{show} Episode {episodeNum}{fileExt}'
-        #Preview the format before committing to changes
-        remake(newName)
-        
+# String pattern to be used to split each filename
+episodeString = 'Episode'
+movieString = 'Movie'
+ovaString = 'OVA'
+specialString = 'Special'
+ovaAndEpisodeString = 'OVA Episode'
+episodeAndOVAstring = 'Episode OVA'
+# List will store the filenames with the new name
+collection = []
+originalFileName = []
+# Counter is needed to ensure each file is actually formatted. In early version, if the file was formatted, that file would be ommitted entirely. Even though in this version this will not happer (or shouldn't), the counter will ensure the same number of files going in will go out reguardless if it has a new name or not.
+count = 0
+formatCount = 0
+missingFiles = count - formatCount
 
-def editName():
-    for f in os.listdir():
-        fileName, fileExt = os.path.splitext(f)
-        fileName = fileName.strip()
-        for word in omitWordList:
-            fileName = fileName.replace(word, '')
-
-        #Split the fileName and Episode number. 
-        show, episodeNum = fileName.split('Episode')
-        #Strip away empty space and fill episodeNum with a zero so shows will play correctly in a playlist
-        show, episodeNum, fileExt = show.strip(), episodeNum.strip().zfill(2),fileExt.strip()
-        #Set the format you want the shows to appear
-        newName = f'{show} Episode {episodeNum}{fileExt}'
-        #Preview the format before committing to changes
-        os.rename(f, newName)
-
-#Change to cwd to the file we need to edit
-def workPath():
-    pattern = input('==>Enter Folder: ')
-    os.chdir(r'{}'.format(pattern))
-
-#Create a list of words and characters to be removed while parsing
-omitWordList = ['English','Dubbed','Watch','cartoons','online','anime','dub','anime',',','Online','-','  ','–','onlin','onli','Wat','Wa','[320x180]']
-
-workPath()
-
-nameCheck()
-
-#After user checks the filenames and make sure editing is good. 
-userInput = input('==>Verify names are good: ')
-
-if userInput == 'y':
-    editName()
+# Format each file
+for f in os.listdir():
+    fname, ext = os.path.splitext(f)
+    ext = ext.strip()
+    originalFileName.append(fname)
+    for word in omit:
+        fname = fname.replace(word, '')
+        if episodeString in fname:
+            show, num = fname.split('Episode')
+            for spec in num:
+                if spec == ',':
+                    num = num.replace(',', '')
+            show, num = show.strip(), num.strip().zfill(2)
+            newName = f'{show} Episode {num}{ext}'
+        if ovaString in fname:
+            show, num = fname.split('OVA')
+            for spec in num:
+                if spec == ',':
+                    num = num.replace(',', '')
+            show, num = show.strip(), num.strip().zfill(2)
+            if num == '00':
+                newName = f'{show} OVA{ext}'
+            else:
+                newName = f'{show} OVA {num}{ext}'
+        if movieString in fname:
+            show, num = fname.split('Movie')
+            show, num = show.strip(), num.strip().zfill(2)
+            if num == '00':
+                newName = f'{show} Movie{ext}'
+            else:
+                newName = f'{show} Movie {num}{ext}'
+        if specialString in fname:
+            show, num = fname.split('Special')
+            show, num = show.strip(), num.strip().zfill(2)
+            newName = f'{show} Special {num}{ext}'
+        if ovaAndEpisodeString in fname:
+            show, num = fname.split('Episode')
+            show, num = show.strip(), num.strip().zfill(2)
+            newName = f'{show} Episode {num}{ext}'
+        if episodeAndOVAstring in fname:
+            show, num, ova = fname.split('Episode')
+            for spec in num:
+                if spec == ',':
+                    num = num.replace(',', '')
+            show, num, ova = show.strip(), num.strip().zfill(2), ova.strip()
+            newName = f'{show} Episode {num}{ext}'
+    collection.append(newName)
+    count += 1
+    print(newName)
+# This will go into os.rename definition
+print('')
+print('Number of show tiltes: %s' % count)
+print('-------------------------')
+for tiles, og in zip(collection, originalFileName):
+    print(og + ' Renamed to:  %s' % tiles)
+    formatCount += 1
+print('')
+print('Total files formatted: %s' % formatCount)
+print('-------------------------')
+if count == formatCount:
+    print('All files in folder are accounted for')
 else:
-    print('Review files in folder. If you felt rename should have been good, you may have to edit more words or characters to omit list')
-
+    print('Number of files missing after format: %s' % missingFiles)
